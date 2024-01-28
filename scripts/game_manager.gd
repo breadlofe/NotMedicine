0137a431470f8bdcdd5e90bd59c4773d7ee0ca0f
GoldenKnight582 <tycobb582@gmail.com>
2024-01-27 19:41:37 -0500

    Pause Menu

---
+++ scripts/game_manager.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var pause_menu = $player.pause_menu
+var paused = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("menu"):
+		toggle_pause()
+
+func toggle_pause():
+	if paused:
+		pause_menu.hide()
+		get_tree().paused = false
+	else:
+		pause_menu.show()
+		pause_menu.on_reveal()
+		get_tree().paused = true
+	paused = !paused
