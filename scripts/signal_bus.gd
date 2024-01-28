62efe5b47ac2a1971b5425ea862fef100ab9a53c
Desmond Craft <dmc112601@gmail.com>
2024-01-27 21:46:49 -0500

    fixed stuipd merge

--- scripts/signal_bus.gd
+++ scripts/signal_bus.gd
@@ -2,8 +2,6 @@ extends Node
 
 signal on_player_attempt_funny(patient)
 signal on_pickup(item_type)
-<<<<<<< Updated upstream
 signal on_horn_fired(stun_time)
-=======
 signal cop_is_spawning
->>>>>>> Stashed changes
+
